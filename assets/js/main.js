5d43f0c79b32e036b7b434ae5ecce35da17ec83b
mihir1337 <mihir.mahamud@gmail.com>
2020-01-18 23:01:56 +0600

    g

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,43 @@
 		});
 	}
 
+	// hero Slider
+	var slider = $(".hero-area");
+	slider.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 0,
+		navText: ['<i class="fa fa-long-arrow-left"></i>', '<i class="fa fa-long-arrow-right"></i>'],
+		nav: false,
+		animateIn: 'fadeIn',
+		autoplay: true,
+		autoPlay: 4000,
+		animateOut: 'fadeOut',
+		dots: false
+	});
+	slider.on('translate.owl.carousel', function () {
+		var layer = $("[data-animation]");
+		layer.each(function () {
+			var s_animation = $(this).data('animation');
+			$(this).removeClass('animated ' + s_animation).css('opacity', '0');
+		});
+	});
+	$("[data-delay]").each(function () {
+		var animation_del = $(this).data('delay');
+		$(this).css('animation-delay', animation_del);
+	});
+	$("[data-duration]").each(function () {
+		var animation_dur = $(this).data('duration');
+		$(this).css('animation-duration', animation_dur);
+	});
+	slider.on('translated.owl.carousel', function () {
+		var layer = slider.find('.owl-item.active').find("[data-animation]");
+		layer.each(function () {
+			var s_animation = $(this).data('animation');
+			$(this).addClass('animated ' + s_animation).css("opacity", "1");
+		});
+	});
+
 	// Activate owlCarousel
 	$('.feature-carousel').owlCarousel({
 		items: 4,
